e3b3dae016e3a60f55b9788063ae0089c52b187b
miaozaiye <miaozaiye@proginn.com>
2017-10-01 21:06:40 +0800

    add sound in G-ball & clock

--- miaozaiye/G-ball.py
+++ miaozaiye/G-ball.py
@@ -1,7 +1,7 @@
 #有重力效果的球
 
-
-from stdpackage import stddraw
+import math
+from stdpackage import stddraw,stdarray,stdaudio
 
 stddraw.setXscale(-1.0,1.0)
 stddraw.setYscale(-1.0,1.0)
@@ -16,14 +16,31 @@ g = -0.001
 f = 0.005
 position1 = [(r1x,r1y)]
 
+SPS = 44100
+CONCERT_A = 330.0
+duration = 0.1
+n = int(duration*SPS)
+pitch = 1
+hz = CONCERT_A*(2**(pitch/12.0))
+
+samples = stdarray.create1D(n+1,0.0)
+for i in range(n+1):
+        samples[i] = math.sin(2.0*math.pi*i*hz/SPS)
+
 changepoint = []
+
 while True:
 
-    if abs(r1x+v1x)+RADIUS>1.0: v1x = -v1x
+    if abs(r1x+v1x)+RADIUS>1.0:
+        v1x = -v1x
+
+        stdaudio.playSamples(samples)
+
 
     v1y +=g
     if abs(r1y + v1y)+RADIUS >1.0:
         v1y = -v1y
+        stdaudio.playSamples(samples)
 
     r1x = r1x+v1x
     r1y = r1y + v1y
